0f28d866e57cb5d903ab30cdab8070490a4e923b
godcigam <133311185+godcigam@users.noreply.github.com>
2026-02-13 14:37:25 -0800

    Create particles-init.js

---
+++ particles-init.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+  particlesJS("particles-js", {
+    particles: {
+      number: { value: 50 },
+      size: { value: 4 },
+      color: { value: "#ffffff" },
+      move: { speed: 3 }
+    }
+  });
+};
